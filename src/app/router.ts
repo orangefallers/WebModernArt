238f898739c665a77010e81cea868a10c24ca9dd
kevin <orangefaller@gmail.com>
2026-08-28 16:39:37 +0800

    add deploy page yaml

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import GameView from '@/views/GameView.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/game', name: 'game', component: GameView },
